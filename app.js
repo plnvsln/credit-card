60f5fa971edf0fc9e54f5d205b24f2d2d552a378
plnvsln <mr.mightypo@gmail.com>
2023-11-23 08:15:36 +0000

    working on js function showError()

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 //show error msg
 
-let button = document.getElementById("btn");
-function showErrorMsg() {
+function showErrorMsg(event) {
+  event.preventDefault;
   let cardInput = document.getElementById("card");
   let dateMonthInput = document.getElementById("date");
   let dateYearInput = document.getElementById("date-YY");
   let cvcInput = document.getElementById("cvc");
-
+  let box1 = document.querySelector(".line2");
+  let box2 = document.querySelector(".card-info-input-1");
+  let box3 = document.querySelector(".card-info-input-2");
+  let box4 = document.querySelector(".card-info-box--2");
   //   alert("hi error");
-
-  if ((dateMonthInput.value = null)) {
-    dateMonthInput.classList.add("show-error");
+  if (cardInput.value) {
+    ("if the letters entered = show mssg ONLY NUMBERS");
+  }
+  if (dateMonthInput.value == null) {
+    box2.classList.add("show-error");
+  }
+  if (dateYearInput.value == null) {
+    box3.classList.add("show-error");
+  }
+  if (cvcInput.value == null) {
+    box4.classList.add("show-error");
   }
 }
-
-// button.addEventListener("click", showErrorMsg());
+let button = document.getElementById("btn");
+button.addEventListener("click", showErrorMsg);
